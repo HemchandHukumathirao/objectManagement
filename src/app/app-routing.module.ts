d2d4cf06d0b39cd4e569d389193ca7eb5f070aed
nikitha2se <nimish.goel@accenture.com>
2018-11-28 15:46:27 +0530

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,31 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ObjectsComponent } from './components/objects/objects.component';
+import { AddObjectComponent } from './components/add-object/add-object.component';
 
-const routes: Routes = [];
+// Parent Routes
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/list',
+    pathMatch: 'full'
+  },
+  {
+    path: 'list',
+    component: ObjectsComponent
+  },
+  {
+    path: 'add',
+    component: AddObjectComponent
+  },
+  {
+    path: 'list/update/:id',
+    component: AddObjectComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { ObjectsComponent } from './components/objects/objects.component';
+import { AddObjectComponent } from './components/add-object/add-object.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, ObjectsComponent, AddObjectComponent],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
